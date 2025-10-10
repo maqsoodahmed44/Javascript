5589f8735c234f584fa29e0de732ff2542c09eda
maqsoodahmed44 <maqsoodah250@gmail.com>
2025-10-11 01:12:59 +0500

    javascript 1st assignment chapter 1 to 3

---
+++ assignment 1/script.js
@@ -0,0 +1,43 @@
+
+// chapter no 1
+
+alert("Error!Please enter a valid password.")
+
+
+
+alert("Welcome to Js land...\n Happy Coding!")
+
+
+
+alert("Welcome to Js land...")
+
+// chapter no 2
+
+let username = "Maqsood Ahmed"
+alert(username)
+
+let age = "23 years old"
+alert(age)
+let certificate = "Certified Mobile Application Development"
+
+
+alert(certificate)
+
+
+
+let pizza = "pizza\n" + "pizz\n" + "piz\n" + "pi\n" + "pi\n" + "p\n"
+alert(pizza)
+let email = "maqsoodah250@gmail.com"
+alert(email)
+
+let marks = "I am trying to learn from the book A smarter way to learn javascript"
+alert(marks)
+let write = "<h1> ya! I can write html content through javascript"
+document.writeln(write)
+
+
+// chapter no 3
+
+let init;
+init = "you have visited this site 13 times"
+alert(init)
