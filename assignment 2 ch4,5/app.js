a6b12de08df8426fc6917bba3c000e1fa68fc70f
maqsoodahmed44 <maqsoodah250@gmail.com>
2025-10-16 00:03:57 +0500

    assignment2

---
+++ assignment 2 ch4,5/app.js
@@ -0,0 +1,44 @@
+// let firstName = "John", lastName = "Doe", age = 30;
+// You can also leave them uninitialized:
+// let a, b, c;
+// console.log(firstName)
+
+
+
+// chapter 4
+
+// question 1
+let variables = "<h1>1)variable statement"
+document.writeln(variables)
+let studentNames = "<h3>maqsood";
+let marks = "<h3>100";
+let isPass = "<h3>true";
+document.writeln(studentNames)
+document.writeln(marks)
+document.writeln(isPass)
+
+// question no 2
+
+// legal
+let a = "<h1>2)legal variables names"
+document.writeln(a)
+let legal = "<h3> myCode, $saylani, saylani_course, userName123, _fullName, "
+document.writeln(legal)
+let b = "<h1>illegal variables names"
+document.writeln(b)
+let illegal = " <h3>var, 12variables, code js, may-be, for"
+document.writeln(illegal)
+
+let heading = "<h1>3)Rules for naming JS variables<h1/>"
+document.writeln(heading)
+
+let names = "<h3> Variable names can only contain letters, digits, underscores and dollar signs. For example $my_1stjavascriptcode"
+document.writeln(names)
+let name = " <h3>Variables must begin with a letter, an underscore, or a dollar sign. For example name, _name or name"
+document.writeln(name)
+let c = " <h3>Variable names are case sensitive."
+document.writeln(c)
+let d = " <h3>Variable names should not be JS reserved words."
+document.writeln(d)
+
+
