78d1e683b29e6aee1c5f8105e388dee1f3731298
maqsoodahmed44 <maqsoodah250@gmail.com>
2025-11-06 01:39:17 +0500

    String method

---
+++ assignment 7 ch 19 to 25/app.js
@@ -0,0 +1,32 @@
+//question no 1
+let firstName = "Maqsood";
+lastName = "Ahmed";
+fullName = "Maqsood Ahmed"
+console.log(fullName);
+//question no 2
+
+let user = prompt("Your Favourite Mobile? ")
+console.log( "My Favourite Mobile is" ,user ,"Lenght of String:" ,user.length)
+
+//question no 03
+
+let find = prompt("find a index number in pakistan");
+let letter = "Pakistan";
+console.log( "String:",letter ,"index of",find , letter.indexOf(find));
+
+//question no 4
+
+let finds = prompt("find a index number in Hello World");
+let letters = "Hello World";
+console.log( "String:",letters ,"index of",finds , letters.lastIndexOf(finds));
+//question no 5
+
+let number = prompt("Find The CHaracterd Of Index in pakistani");
+let enter = "pakistani"
+let result = enter[number];
+console.log("String:" ,enter,"Character at index", " " + number + ":" + result)
+
+//question no 6
+let firstNames = "Maqsood";
+lastNames = "Ahmed";
+console.log(firstNames + " " + lastNames)
